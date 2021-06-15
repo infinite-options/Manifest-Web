a9c689ecfbe889f2798491de4ca1792e8481715c
AjiethVenkat <AjiethVenkat@hotmail.com>
2021-06-15 17:36:04 -0400

    Added Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+/* Importing the custom pages as each components */
+import { Navigation } from "./component/navigation";
+import { Home } from "./Home/Home";
+import { Login } from "./Login/Login";
+
+/* Main function for all the pages and elements */
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+      <div>
+
+        <Link to="/login">Login </Link>
+
+        <Link to="/">Home</Link>
+
+        <Navigation />
+
+        <Switch>
+
+          <Route path="/">
+
+            <Home />
+
+          </Route>
+
+          <Route path="/login">
+
+            <Login />
+
+          </Route>
+
+        </Switch>
+
+      </div>
+
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Login/Login.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import { useHistory } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+
+/* Custom Hook to make styles */
+const useStyles = makeStyles({
+
+   
+});
+
+/* Navigation Bar component function */
+ export function Login(){
+
+    const classes = useStyles();
+
+    return(
+
+        <div style={{width:'100%', height:'100vh', backgroundColor:'#889AB5'}}>
+            Log In
+        </div>
+     
+    )
+}
+
